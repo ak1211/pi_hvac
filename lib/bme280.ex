5d9168d1b9cb2d9ea4b8755fd84319121c5fac7d
ak1211 <ak1211@mail.ak1211.com>
2019-02-21 21:34:00 +0900

    Update LICENSE

--- lib/bme280.ex
+++ lib/bme280.ex
@@ -1,3 +1,21 @@
+#
+# PiHVAC <https://github.com/ak1211/pi_hvac>
+# Copyright 2019 Akihiro Yamamoto
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# module: Bosch BME280 デバイスドライバ
+
 defmodule BME280 do
   @moduledoc """
   Bosch BME280 Temperature / Pressure / Humidity sensor module driver on the Raspberry Pi.
